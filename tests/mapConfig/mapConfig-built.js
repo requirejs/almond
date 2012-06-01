96d53ae0be4f7dcccb55a55ee823acdaff32c6f5
jrburke <jrburke@gmail.com>
2012-05-31 22:30:55 -0700

    Rev version for 0.1.1 release

--- tests/mapConfig/mapConfig-built.js
+++ tests/mapConfig/mapConfig-built.js
@@ -1,5 +1,5 @@
 /**
- * almond 0.1.0+ Copyright (c) 2011, The Dojo Foundation All Rights Reserved.
+ * almond 0.1.1 Copyright (c) 2011, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/almond for details
  */
