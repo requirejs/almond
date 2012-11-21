c974dc9e1b4f906066112cc44a501f1fd74a8f79
jrburke <jrburke@gmail.com>
2012-11-21 14:05:28 -0800

    Rev for 0.2.1 release

--- almond.js
+++ almond.js
@@ -1,5 +1,5 @@
 /**
- * almond 0.2.0+ Copyright (c) 2011-2012, The Dojo Foundation All Rights Reserved.
+ * almond 0.2.1 Copyright (c) 2011-2012, The Dojo Foundation All Rights Reserved.
  * Available via the MIT or new BSD license.
  * see: http://github.com/jrburke/almond for details
  */
