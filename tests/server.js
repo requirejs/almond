f0a6401e15ac17dfbf0951101974bd70af57b764
christoph <christoph@neochic.de>
2015-02-26 00:30:23 +0100

    * fixed static connect server to make tests on travis CI run again

--- tests/server.js
+++ tests/server.js
@@ -1,3 +1,6 @@
-var connect = require('connect');
-connect.createServer(connect.static(__dirname + '/..')).listen(1986);
+var connect = require('connect'),
+    serveStatic = require('serve-static');
+var server = connect();
+server.use(serveStatic(__dirname + '/..'));
+server.listen(1986);
 require('fs').writeFileSync(__dirname + '/pid.txt', process.pid);
